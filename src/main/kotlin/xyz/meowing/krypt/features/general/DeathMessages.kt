4d1a1fa2ea6cec81792c462334e7f8f649194219
aurielyn <aurielyn.dev@gmail.com>
2025-12-04 23:19:17 +0530

    Feat: Death messages

---
+++ src/main/kotlin/xyz/meowing/krypt/features/general/DeathMessages.kt
@@ -0,0 +1,28 @@
+package xyz.meowing.krypt.features.general
+
+import xyz.meowing.knit.api.KnitChat
+import xyz.meowing.krypt.annotations.Module
+import xyz.meowing.krypt.api.location.SkyBlockIsland
+import xyz.meowing.krypt.events.core.DungeonEvent
+import xyz.meowing.krypt.features.Feature
+
+@Module
+object DeathMessages : Feature(
+    "deathMessages",
+    "Death messages",
+    "Sends messages on player deaths",
+    "General",
+    island = SkyBlockIsland.THE_CATACOMBS
+) {
+    private val message by config.textInput("Message", "{name} died! {count} deaths yet.")
+
+    override fun initialize() {
+        register<DungeonEvent.Player.Death> { event ->
+            val message = message
+                .replace("{name}", event.player.name)
+                .replace("{count}", event.player.deaths.toString())
+
+            KnitChat.fakeMessage(message)
+        }
+    }
+}
