4f25b108407c82f7974bcd8da26ad3f77cef4998
aurielyn <aurielyn.dev@gmail.com>
2025-12-06 13:09:54 +0530

    Feat: Hide after leap

--- src/main/kotlin/xyz/meowing/krypt/features/alerts/LeapAlert.kt
+++ src/main/kotlin/xyz/meowing/krypt/features/alerts/LeapAlert.kt
@@ -2,9 +2,12 @@ package xyz.meowing.krypt.features.alerts
 
 import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
 import xyz.meowing.knit.api.KnitChat
+import xyz.meowing.knit.api.KnitPlayer
+import xyz.meowing.knit.api.scheduler.TimeScheduler
 import xyz.meowing.krypt.annotations.Module
 import xyz.meowing.krypt.api.location.SkyBlockIsland
 import xyz.meowing.krypt.events.core.ChatEvent
+import xyz.meowing.krypt.events.core.RenderEvent
 import xyz.meowing.krypt.features.Feature
 
 @Module
@@ -16,7 +19,9 @@ object LeapAlert : Feature(
     island = SkyBlockIsland.THE_CATACOMBS
 ) {
     private val leapRegex = "^You have teleported to (.+)".toRegex()
+    private val hideAfterLeap by config.switch("Hide after leap")
     private val message by config.textInput("Message",  "Leaping to {name}")
+    private var hide = false
 
     override fun initialize() {
         register<ChatEvent.Receive> { event ->
@@ -25,6 +30,18 @@ object LeapAlert : Feature(
                 .replace("{name}", result.groupValues[1])
 
             KnitChat.sendCommand("pc $message")
+            hide = true
+            TimeScheduler.schedule(3000) {
+                hide = false
+            }
+        }
+
+        register<RenderEvent.Entity.Pre> { event ->
+            if (!hideAfterLeap) return@register
+            if (!hide) return@register
+            if (event.entity.id == KnitPlayer.player?.id) return@register
+
+            event.cancel()
         }
     }
 }
